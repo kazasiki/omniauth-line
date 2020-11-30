71171853c556bc8cfdd37abad47c71afc965eba4
seokjoon.yun <seokjoon.yun@gmail.com>
2020-11-30 09:56:09 +0900

    apply omniauth-oauth2 > 1.7

--- lib/omniauth/strategies/line.rb
+++ lib/omniauth/strategies/line.rb
@@ -19,6 +19,19 @@ module OmniAuth
         super
       end
 
+      # Correct redirect_uri_mismatch issues related to callback_uri through path match.
+      # NOTE If we're using code from the signed request then FB sets the redirect_uri to '' during the authorize
+      #      phase and it must match during the access_token phase:
+      #      https://github.com/facebook/facebook-php-sdk/blob/master/src/base_facebook.php#L477
+      def callback_url
+        if @authorization_code_from_signed_request_in_cookie
+          ''
+        else
+          # callback url ignorance issue from https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+          options[:callback_url] || (full_host + script_name + callback_path)
+        end
+      end
+
       uid { raw_info['userId'] }
 
       info do
